87204960849d9ad737ef38f36daaa9ee50852ccd
Angel Cortez <Angel.Cortez@cybermutts.com>
2021-11-02 01:44:26 -0400

    'workinprogress'

---
+++ encrpyt.py
@@ -0,0 +1,25 @@
+import random
+from string import ascii_lowercase
+
+sample_wordlist = ["terces", "twosix", "yofour"]
+length = len(sample_wordlist)
+sample_input = "secret"
+
+step = random.randint(1, length - 1)
+
+code_word = sample_wordlist[step]
+
+LETTERS = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)}
+
+
+def alphabet_position(text):
+    text = text.lower()
+
+    numbers = [LETTERS[character] for character in text if character in LETTERS]
+
+    return ' '.join(numbers)
+
+
+secret_word_as_number = alphabet_position(sample_input)
+code_word_as_number = alphabet_position(code_word)
+
